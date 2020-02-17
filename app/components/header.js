87f5b35dc295f8cf61dd64333ca6f46e21a579f2
Sylvain DELESCLUSE <sylvain.delescluse@ext.leroymerlin.fr>
2020-02-17 12:14:05 +0100

    feat(header): React component managing SSR exception

--- app/components/header.js
+++ app/components/header.js
@@ -1,32 +1,60 @@
 import Link from 'next/link';
+import React from 'react';
 import { jsx } from '@emotion/core';
 
 /** @jsx jsx */
 
-export default () => (
-  <header
-    css={{
-      display: 'flex',
-      alignItems: 'center',
-      margin: '48px 0',
-    }}
-  >
-    <img src="public/images/syllium-diaph.svg" alt="logo" css={{width: '80px'}} />
-    <Link href="/" passHref>
-      <a css={{
-        color: 'hsl(200, 20%, 50%)',
-        margin: '0 0 0 1rem',
-        lineHeight: '2.2rem',
-        fontSize: '2em',
-        ':hover': {
-          textDecoration: 'none'
-        },
-      }}>Syllium Photography Blog</a>
-    </Link>
-    {/*
-    <Link href="/post/new" passHref>
-      <a css={{ color: 'hsl(200, 20%, 50%)', cursor: 'pointer' }}>+ Add Post</a>
-    </Link>
-    */}
-  </header>
-);
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ssrDone: false
+    }
+  }
+  componentDidMount() {
+    this.setState({ ssrDone: true, online: navigator.onLine })
+  }
+  render() {
+    if(!this.state.ssrDone) {
+      return (
+        <div>loading...</div>
+      )
+    }
+
+    const logoPath = 'public/images/syllium-diaph.svg';
+    let absLogoPath = logoPath;
+    if (typeof window !== 'undefined') {
+      absLogoPath = `${window.location.origin}/${logoPath}`;
+    }
+
+    return (
+      <header
+        css={{
+          display: 'flex',
+          alignItems: 'center',
+          margin: '48px 0',
+        }}
+      >
+        <img src={absLogoPath} alt="logo" css={{width: '80px'}} />
+        <Link href="/" passHref>
+          <a css={{
+            color: 'hsl(200, 20%, 50%)',
+            margin: '0 0 0 1rem',
+            lineHeight: '2.2rem',
+            fontSize: '2em',
+            ':hover': {
+              textDecoration: 'none'
+            },
+          }}>Syllium Photography Blog</a>
+        </Link>
+        {/*
+        <Link href="/post/new" passHref>
+          <a css={{ color: 'hsl(200, 20%, 50%)', cursor: 'pointer' }}>+ Add Post</a>
+        </Link>
+        */}
+      </header>
+    );
+  }
+}
+
+export default Header;
