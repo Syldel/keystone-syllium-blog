66ff491e23665377bbb377d28e68ed590407427b
Sylvain DELESCLUSE <sylvain.delescluse@ext.leroymerlin.fr>
2020-02-17 18:34:24 +0100

    feat(images): add favicon

--- app/templates/layout.js
+++ app/templates/layout.js
@@ -24,6 +24,7 @@ const Layout = ({ children }) => (
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <title>Syllium Photography Blog</title>
+      <link rel="icon" href="../public/images/favicon.ico" />
       <link rel="stylesheet" type="text/css" href="../public/styles/bootstrap.min.css"></link>
     </Head>
     <div
