44996c9317308882712df660e0d7da3802de57ec
Sylvain DELESCLUSE <sylvain.delescluse@ext.leroymerlin.fr>
2020-02-26 13:59:11 +0100

    feat(config): cloudinary default env values

--- schema.js
+++ schema.js
@@ -41,16 +41,16 @@ const dev = process.env.NODE_ENV !== 'production';
 //});
 
 const cloudinaryAdapter = new CloudinaryAdapter({
-  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-  apiKey: process.env.CLOUDINARY_KEY,
-  apiSecret: process.env.CLOUDINARY_SECRET,
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME || '',
+  apiKey: process.env.CLOUDINARY_KEY || '',
+  apiSecret: process.env.CLOUDINARY_SECRET || '',
   folder: 'keystone-syllium-blog/post',
 });
 
 const avatarCloudinaryAdapter = new CloudinaryAdapter({
-  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-  apiKey: process.env.CLOUDINARY_KEY,
-  apiSecret: process.env.CLOUDINARY_SECRET,
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME || '',
+  apiKey: process.env.CLOUDINARY_KEY || '',
+  apiSecret: process.env.CLOUDINARY_SECRET || '',
   folder: 'keystone-syllium-blog/avatar',
 });
 
