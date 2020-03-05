c4c43872f69c9a99e053d8933ae1f4b6d522de2c
Sylvain DELESCLUSE <sylvain.delescluse@ext.leroymerlin.fr>
2020-03-05 11:02:38 +0100

    fix(sitemap): generate new xml at each calls

--- app/pages/api/sitemap.xml.js
+++ app/pages/api/sitemap.xml.js
@@ -1,9 +1,5 @@
 import { request } from 'graphql-request';
 
-let xml = '';
-xml += '<?xml version="1.0" encoding="UTF-8"?>';
-xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-
 const SITEMAP_QUERY = `{
   allPosts (
     where: {status: published}
@@ -13,17 +9,26 @@ const SITEMAP_QUERY = `{
 }`;
 
 const Sitemap = (req, res) => {
-  /*
+  console.log('Sitemap - 1. req.hostname', req.hostname);
   let hostname = req.hostname;
   if (req.headers && req.headers.host) {
     hostname = req.headers.host;
   }
-  const requestUrl = `${req.protocol}://${hostname}/`;
-  const siteOrigin = `https://${hostname}/`;
-  */
+  console.log('Sitemap - 2. hostname', hostname);
+  let protocol = req.protocol || 'https';
+  console.log('Sitemap - 1. protocol', protocol);
+  if (req.headers && req.headers['x-forwarded-proto']) {
+    protocol = req.headers['x-forwarded-proto'];
+  }
+  console.log('Sitemap - 2. protocol', protocol);
+  //const requestUrl = `${protocol}://${hostname}/`;
+  //const siteOrigin = `https://${hostname}/`;
 
   const requestUrl = `https://blog.syllium-photography.fr/`;
   const siteOrigin = requestUrl;
+
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>';
+  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
   xml += `<url>`;
   xml += `<loc>${siteOrigin}</loc>`;
